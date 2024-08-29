61fc69326899d353ed87cf25d070453e09e5291f
Shunki Takami <tkmnet@users.noreply.github.com>
2024-08-29 14:01:53 +0000

    update server.sh

--- server.sh
+++ server.sh
@@ -2,6 +2,5 @@
 #
 
 
-mvn install package -DskipTests
-java -Xmx1G -jar target/barefoot-0.1.5-matcher-jar-with-dependencies.jar config/server.properties local/database.properties
+mvn install package -DskipTests && java -Xmx1G -jar target/barefoot-0.1.5-matcher-jar-with-dependencies.jar config/server.properties local/database.properties
 
